461b7865a2aeabe72c1277784802f2e300d892af
azzadri007 <azzadri007@gmail.com>
2020-05-17 19:16:38 +0100

    prefetch added

--- scripting/page3/onLoad.js
+++ scripting/page3/onLoad.js
@@ -1,12 +1,12 @@
 
 window.onload = function() {
   if(!window.location.hash) {
-      window.location = window.location + '#loaded';
+      window.location = window.location + '#reloaded';
       window.location.reload();
   }
 }
 $(function (){
-  $('.p5Canvas').css({'position':'absolute','left':'50%','margin-left':'-200px'});
+  $('.p5Canvas').css({'position':'absolute','left':'50%'});
   $('button').delay(2000).animate({'opacity':'1'},1000);
   $(".p5Canvas").animate({"top":"0px"}, 2000);
     
