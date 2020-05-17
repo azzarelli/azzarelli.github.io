5a4b48583ea93c477686fa53970e1f46f9d05cc8
azzadri007 <azzadri007@gmail.com>
2020-05-17 19:06:02 +0100

    prefetch added

--- scripting/page3/onLoad.js
+++ scripting/page3/onLoad.js
@@ -1,4 +1,10 @@
 
+window.onload = function() {
+  if(!window.location.hash) {
+      window.location = window.location + '#loaded';
+      window.location.reload();
+  }
+}
 $(function (){
   $('.p5Canvas').css({'position':'absolute','left':'50%','margin-left':'-200px'});
   $('button').delay(2000).animate({'opacity':'1'},1000);
