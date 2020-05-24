40f925d2ba2be83bd1321a66569d8c59adbfe83f
azzadri007 <azzadri007@gmail.com>
2020-05-24 12:29:39 +0100

    center game

--- scripting/page3/onLoad.js
+++ scripting/page3/onLoad.js
@@ -6,7 +6,6 @@ window.onload = function() {
   }
 }
 $(function (){
-  $('.p5Canvas').css({'position':'absolute','left':'50%'});
   $('button').delay(2000).animate({'opacity':'1'},1000);
   $(".p5Canvas").animate({"top":"0px"}, 2000);
     
